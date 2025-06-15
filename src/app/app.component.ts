6165b7eecc86a01fcc43fabf8550707ab1e9c0c2
O-B-I-s <86371109+O-B-I-s@users.noreply.github.com>
2025-06-14 22:50:33 -0600

    Initial frontend commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EventsComponent } from './components/events/events.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, EventsComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Frontend';
---
+++ src/app/components/sales-summary/sales-summary.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sales-summary',
+  imports: [],
+  templateUrl: './sales-summary.component.html',
+  styleUrl: './sales-summary.component.css'
+})
+export class SalesSummaryComponent {
+
+}
